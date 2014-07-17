ea624a52f6d4a89787e9042cd65f500d1cc3584e
Ximo <Ximo@Mac-Pro-de-Ximo.local>
2014-07-17 14:26:59 +0200

    Initial Commit

---
+++ UITableViewController Solution/XAViewController.h
@@ -0,0 +1,13 @@
+//
+//  XAViewController.h
+//  UITableViewController Solution
+//
+//  Created by Ximo on 17/07/14.
+//  Copyright (c) 2014 Ximo Alloza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XAViewController : UIViewController
+
+@end
---
+++ UITableViewController SolutionTests/UITableViewController_SolutionTests.m
@@ -0,0 +1,34 @@
+//
+//  UITableViewController_SolutionTests.m
+//  UITableViewController SolutionTests
+//
+//  Created by Ximo on 17/07/14.
+//  Copyright (c) 2014 Ximo Alloza. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UITableViewController_SolutionTests : XCTestCase
+
+@end
+
+@implementation UITableViewController_SolutionTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
